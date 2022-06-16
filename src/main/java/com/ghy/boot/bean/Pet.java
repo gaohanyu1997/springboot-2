6f6005cdc2c06c47ffb54f9731d8aad89a5e7a4f
gaohanyu <2405662372@qq.com>
2022-06-17 01:27:13 +0800

    SpringBoot2初始化向导-yaml的用法

---
+++ src/main/java/com/ghy/boot/bean/Pet.java
@@ -0,0 +1,9 @@
+package com.ghy.boot.bean;
+import lombok.Data;
+import lombok.ToString;
+@Data
+@ToString
+public class Pet {
+    private String name;
+    private Double weight;
+}
---
+++ src/main/java/com/ghy/boot/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.ghy.boot.controller;
+import com.ghy.boot.bean.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+    @Autowired
+    Person person;
+    @RequestMapping("/person")
+    public Person person(){
+        String userName = person.getUserName();
+        System.out.println(userName);
+        return person;
+    }
+}
